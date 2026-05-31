83e002c7c4ba9e0d4699c17d787f1a9240d9558a
Alexis Morelle <alexis.morelle@gmail.com>
2026-05-31 07:23:16 +0000

    feat: more random quiz

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -6,7 +6,14 @@ import styles from './Quiz.module.css'
 import SEO from '../components/SEO'
 import Icon from '../components/Icon'
 
-function shuffle(arr) { return [...arr].sort(() => Math.random() - .5) }
+function shuffle(arr) {
+  const a = [...arr]
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]]
+  }
+  return a
+}
 
 export default function Quiz() {
   const [allFigures, setAllFigures] = useState([])
